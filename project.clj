fe8f19495a1ec001882ef0abf9e9aba13f68b788
Chris Perkins <chrisperkins99@gmail.com>
2011-10-01 17:54:22 -0400

    clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-ex "1.0.0"
   :description "Provides a simple Exception class that can hold arbitrary data, instead of just a string."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :aot [clj-ex.core])
